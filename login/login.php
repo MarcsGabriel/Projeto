b52ddc7ccef961bb202fee6b2825b088b0ae8dbf
Marcos Gabriel <gabrielmfcct@hotmail.com>
2024-02-05 15:33:52 -0300

    next update

--- login/login.php
+++ login/login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylelogin.css">
-    <title>Login GEP</title>
+    <title>Login GEPI</title>
 </head>
 <body>
     <form action="testeLogin.php" method="POST"> 
