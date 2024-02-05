ceef3c93ccb83126138662707022244f506aad36
Marcos Gabriel <88412675+MarcsGabriel@users.noreply.github.com>
2024-02-05 15:35:48 -0300

    primeira atualização login.php

--- login/login.php
+++ login/login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylelogin.css">
-    <title>Login GEPI</title>
+    <title>Login GE</title>
 </head>
 <body>
     <form action="testeLogin.php" method="POST"> 
